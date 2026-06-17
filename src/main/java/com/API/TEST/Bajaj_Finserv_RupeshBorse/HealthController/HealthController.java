a834567615915758c19c0d2bf2065c88814a0e97
RupeshBorse17 <rupeshborse2005@gmail.com>
2026-06-17 17:24:23 +0530

    Added health endpoint

---
+++ src/main/java/com/API/TEST/Bajaj_Finserv_RupeshBorse/HealthController/HealthController.java
@@ -0,0 +1,18 @@
+package com.API.TEST.Bajaj_Finserv_RupeshBorse.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, String> health() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "OK");
+        return response;
+    }
+}
--- src/main/java/com/API/TEST/Bajaj_Finserv_RupeshBorse/dto/ResponseDto.java
+++ src/main/java/com/API/TEST/Bajaj_Finserv_RupeshBorse/dto/ResponseDto.java
@@ -1,5 +1,7 @@
 package com.API.TEST.Bajaj_Finserv_RupeshBorse.dto;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.List;
 import java.util.Map;
 
